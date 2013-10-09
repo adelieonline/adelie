c1d3c86a6cc6694be1ecfa1d3b6e915b1fd65541
Russell Stern <skier2k5@gmail.com>
2013-10-08 23:16:41 -0400

    [FIX] fixed bug in update cart code

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -66,9 +66,7 @@ class CartController < ApplicationController
   def update
     cart_item_id = params[:cart_item_id].presence
     quantity = params[:quantity].presence
-    console_id = params[:console_id].presence
-    console = Console.where(:id => console_id).first
-    if cart_item_id.present? && quantity.present? && console.present?
+    if cart_item_id.present? && quantity.present?
       if current_user
         cart = Cart.where(:user_id => current_user.id, :checked_out => false).first
         if cart.nil?
@@ -80,7 +78,6 @@ class CartController < ApplicationController
               cart_item.delete
             else
               cart_item.quantity = quantity
-              cart_item.console_id = console.id
               cart_item.save
             end
             return render :json => "Updated"
@@ -97,7 +94,6 @@ class CartController < ApplicationController
               session[:cart].delete(cart_item)
             else
               cart_item[:quantity] = quantity.to_i
-              cart_item[:console_id] = console.id
             end
             return render :json => "Updated"
           end
