fe94679846ca924da29b5d382ad916252db91cb3
Russell Stern <skier2k5@gmail.com>
2013-10-08 20:42:55 -0400

    [FIX] pictures now are uploaded in thumb and medium verisions

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,10 +2,8 @@ class Picture < ActiveRecord::Base
   include Concerns::ReportingRecord
   attr_accessible :caption, :created_ts, :updated_ts, :picture, :product_id, :picture_file_name, :picture_content_type
   has_attached_file :picture,
-                    :styles =>  { :medium => "300x300>", :thumb => "100x100>" },
-                    :default_url => "/images/:style/missing.png",
-                    :url => "/assets/images/:basename.:extension",
-                    :path => ":rails_root/public/assets/images/:basename.:extension"
+                    :styles =>  { :medium => "500x500>", :thumb => "100x100>" },
+                    :default_url => "/images/:style/missing.png"
 
   belongs_to :product
 end
