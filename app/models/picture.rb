51318b647eb2f6f1d69be5702677872919f79225
Russell Stern <skier2k5@gmail.com>
2013-10-10 21:44:13 -0400

    Resized pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,7 @@ class Picture < ActiveRecord::Base
   include Concerns::ReportingRecord
   attr_accessible :caption, :created_ts, :updated_ts, :picture, :product_id, :picture_file_name, :picture_content_type
   has_attached_file :picture,
-                    :styles =>  { :medium => "500x500>", :thumb => "100x100>" },
+                    :styles =>  { :medium => "750x500>", :thumb => "100x100>" },
                     :default_url => "/images/:style/missing.png"
 
   belongs_to :product
