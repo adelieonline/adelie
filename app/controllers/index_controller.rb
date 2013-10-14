f97369d62e4ee49a76fcd5093532be9a0622adf2
Russell Stern <skier2k5@gmail.com>
2013-10-13 22:43:40 -0400

    Added upcoming vs current products

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,22 @@
 class IndexController < ApplicationController
 
   def index
-    @current_products = Product.all
+    all_products = Product.all
+    @current_products = []
+    @upcoming_products = []
+    all_products.each do |product|
+      if product.is_active?
+        @current_products.append(product)
+      elsif product.is_upcoming?
+        @upcoming_products.append(product)
+      end
+    end
+    if @upcoming_products.length == 0
+      @upcoming_products = nil
+    end
+    if @current_products.length == 0
+      @current_products = nil
+    end
   end
 
   def howitworks
